0a66f50a8856eaed6c17264efc95015a6344a9a8
samaa <samaasoltan6@gmail.com>
2020-12-06 11:31:40 +0200

    Initial Commit

---
+++ SCToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SCToDoList
+//
+//  Created by samaa on 12/6/20.
+//  Copyright © 2020 samaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
